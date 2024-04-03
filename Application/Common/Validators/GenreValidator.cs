66a85830463c72b3d06a8a0558dc977e9222f40f
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-04-03 13:55:10 +0500

    added GenreValidator

---
+++ Application/Common/Validators/GenreValidator.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Common.Validators;
+
+public class GenreValidator : AbstractValidator<Genre>
+{
+    public GenreValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Janr bo'sh bo'lmasin")
+            .Length(4, 20).WithMessage("4 yoki 20 ta harfdan iborat bo'lsin");
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("kino haqida ma'lumot kiriting");
+    }
+}
