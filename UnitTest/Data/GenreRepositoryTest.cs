c4470dae9303821ab418727dd8f40ec9907dc83d
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-05-17 13:53:31 +0500

    Testlar yozish uchun )

---
+++ UnitTest/Data/GenreRepositoryTest.cs
@@ -0,0 +1,37 @@
+﻿using Data.DbContexts;
+using Data.Interfaces;
+using Data.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTest.Data;
+
+public class GenreRepositoryTest
+{
+    DbContextOptions<AppDbContext> options =
+        new DbContextOptionsBuilder<AppDbContext>()
+        .UseInMemoryDatabase(databaseName: "movie-ntv")
+        .Options;
+
+    AppDbContext dbContext;
+    IUnitOfWork unitOfWork;
+
+    [TearDown]
+    public void SetUp()
+    {
+        dbContext = new AppDbContext(options);
+        unitOfWork = new UnitOfWork(dbContext);
+    }
+
+    [Test]
+    public async Task AddAsync()
+    {
+        var genres = new Genre() { Name = "Test", Description = "Yana Test", CreatedAt = DateTime.UtcNow };
+        await unitOfWork.Genre.CreateAsync(genres);
+
+        var result = await dbContext.Genres.FirstOrDefaultAsync(p => p.Name == genres.Name);
+
+        Assert.That(result, Is.Not.Null);
+    }
+}
+
---
+++ UnitTest/Service/AccountServiceTest.cs
@@ -0,0 +1,6 @@
+﻿namespace UnitTest.Service
+{
+    internal class AccountServiceTest
+    {
+    }
+}
